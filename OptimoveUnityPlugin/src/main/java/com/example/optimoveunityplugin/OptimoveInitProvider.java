bebb874fc05465717958dbc20db53980cbc67e7d
Tom Barzilay <tom_b@optimove.com>
2022-10-09 13:56:04 +0300

    added optimoveInitProvider

---
+++ OptimoveUnityPlugin/src/main/java/com/example/optimoveunityplugin/OptimoveInitProvider.java
@@ -0,0 +1,50 @@
+package com.example.optimoveunityplugin;
+
+import android.app.Application;
+import android.content.ContentProvider;
+import android.content.ContentValues;
+import android.database.Cursor;
+import android.net.Uri;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+
+import com.optimove.android.Optimove;
+import com.optimove.android.OptimoveConfig;
+
+public class OptimoveInitProvider extends ContentProvider {
+    @Override
+    public boolean onCreate() {
+        Application app =(Application) this.getContext().getApplicationContext();
+        Optimove.initialize(app,new OptimoveConfig.Builder("YOUR_OPTIMOVE_CREDENTIALS", "YOUR_OPTIMOVE_MOBILE_CREDENTIALS").build());
+        return  true;
+    }
+
+    @Nullable
+    @Override
+    public Cursor query(@NonNull Uri uri, @Nullable String[] strings, @Nullable String s, @Nullable String[] strings1, @Nullable String s1) {
+        return null;
+    }
+
+    @Nullable
+    @Override
+    public String getType(@NonNull Uri uri) {
+        return null;
+    }
+
+    @Nullable
+    @Override
+    public Uri insert(@NonNull Uri uri, @Nullable ContentValues contentValues) {
+        return null;
+    }
+
+    @Override
+    public int delete(@NonNull Uri uri, @Nullable String s, @Nullable String[] strings) {
+        return 0;
+    }
+
+    @Override
+    public int update(@NonNull Uri uri, @Nullable ContentValues contentValues, @Nullable String s, @Nullable String[] strings) {
+        return 0;
+    }
+}
