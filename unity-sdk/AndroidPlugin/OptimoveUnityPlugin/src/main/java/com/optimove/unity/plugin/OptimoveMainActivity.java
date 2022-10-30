4da007d6f4dfe455e97faa0091870b8c65a070e1
Tom Barzilay <tom_b@optimove.com>
2022-10-30 16:06:11 +0200

    added onNewIntent override

--- unity-sdk/AndroidPlugin/OptimoveUnityPlugin/src/main/java/com/optimove/unity/plugin/OptimoveMainActivity.java
+++ unity-sdk/AndroidPlugin/OptimoveUnityPlugin/src/main/java/com/optimove/unity/plugin/OptimoveMainActivity.java
@@ -1,6 +1,9 @@
 package com.optimove.unity.plugin;
+
 import com.google.firebase.MessagingUnityPlayerActivity;
 import com.optimove.android.Optimove;
+
+import android.content.Intent;
 import android.os.Bundle;
 
 public class OptimoveMainActivity extends MessagingUnityPlayerActivity {
@@ -14,5 +17,11 @@ public class OptimoveMainActivity extends MessagingUnityPlayerActivity {
     public void onWindowFocusChanged(boolean hasFocus) {
         super.onWindowFocusChanged(hasFocus);
         Optimove.getInstance().seeInputFocus(hasFocus);
+    }
+
+    @Override
+    protected void onNewIntent(Intent intent) {
+        super.onNewIntent(intent);
+        Optimove.getInstance().seeIntent(intent);
     }
 }
